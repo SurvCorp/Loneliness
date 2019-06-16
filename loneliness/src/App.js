dc3d1a98db6289415011662a75366f44e84ca2a6
Pedro Basilio <artiumdominus@protonmail.com>
2019-06-16 01:21:29 -0300

    Diretório prototype

--- loneliness/src/App.js
+++ loneliness/src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+
+import Login from './Login.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ui segment">
+      Bem vindo! você está logado.
     </div>
   );
 }
 
-export default App;
+function AppRouter() {
+  return (
+    <Router>
+      <div style={{height: "100%"}}>
+        
+        {/*
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/home/">App</Link>
+            </li>
+          </ul>
+        </nav>
+        */}
+        
+        <Route path="/" exact render={() => <Login auth={fakeAuth}/>}/>
+        <PrivateRoute path="/home/" component={App} />
+      </div>
+    </Router>
+  );
+}
+
+const fakeAuth = {
+  isAuthenticated: true,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props => fakeAuth.isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{pathname: "/", state: {from: props.location}}}
+        />
+      )
+      }
+    />
+  );
+}
+
+export default AppRouter;
---
+++ loneliness/src/Login.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './Login.css';
+import {Redirect} from 'react-router-dom';
+//import $ from 'jquery';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { redirectToReferrer: false };
+    }
+    
+
+    login = () => {
+        this.props.auth.authenticate(
+            () => this.setState({ redirectToReferrer: true })
+        );
+    }
+
+    render() {
+        if (this.state.redirectToReferrer) return <Redirect to={{pathname: "/home"}}/>
+
+        return (
+            <div className="ui middle aligned center aligned grid">
+                <div className="column">
+                    <h2 className="ui inverted image header">
+                        <img src="crow-grey-silhouette.svg" className="image" alt="Loneliness logo" />
+                        <div className="content">
+                            Log-in to your Loneliness
+                        </div>
+                    </h2>
+                    <form className="ui large form">
+                        <div className="ui stacked segment">
+                            <div className="field">
+                                <div className="ui left icon input">
+                                    <i className="user icon"></i>
+                                    <input type="text" name="username" placeholder="Username or email address"/>
+                                </div>
+                            </div>
+                            <div className="field">
+                                <div className="ui left icon input">
+                                    <i className="lock icon"></i>
+                                    <input type="password" name="password" placeholder="Password"/>
+                                </div>
+                            </div>
+
+                            <div className="ui fluid large black submit button" onClick={this.login}>Login</div>
+                        </div>
+
+                        <div className="ui error message"></div>
+
+                    </form>
+
+                    <div className="ui message">
+                        New to Loneliness? <a href="#">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
